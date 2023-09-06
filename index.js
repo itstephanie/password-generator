01dcf9f5ecae66665e01041d31b053d5d4ad9bff
itstephanie <141762567+itstephanie@users.noreply.github.com>
2023-09-07 02:44:27 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]; 
+
+let generateButton = document.getElementById("generate-btn")
+let passOne = document.getElementById("pass-one")
+let passTwo = document.getElementById("pass-two")
+let passwordLength = 8 
+let messageEl = document.getElementById("message-el")
+
+
+function getRandomCharacter(){
+  let newPassword = ""
+  for (let i = 0; i < passwordLength; i++) {
+       let pass = Math.floor(Math.random() * characters.length);
+       newPassword += characters[pass];
+  }
+  
+   passOne.textContent = newPassword
+   passTwo.textContent = newPassword
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
